5c9552109c4458617a1132d9a64a74335707d538
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-07-08 15:12:30 -0500

    MVP + Stretch #1 completed

--- IWannaBeBlue/ViewController.swift
+++ IWannaBeBlue/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Globals
+    
+    //Outlets
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //Actions
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        view.backgroundColor = .blue
+        textLabel.text = "It's not easy being green"
+        textLabel.textColor = .green
+    }
+    @IBAction func backPressed(_ sender: UIButton) {
+        view.backgroundColor = .white
+        textLabel.text = "What color am I?"
+        textLabel.textColor = .black
+    }
+    
 }
 
